5f84f7565f37ffc5819e7ea2c6577e41fc1405db
stchwang <hsi05200@gmail.com>
2020-11-25 00:01:29 +0900

    URL을 이해하고 query string을 활용해  메뉴링크 클릭시 title값을 가져옴

---
+++ syntax/variable2.js
@@ -0,0 +1,5 @@
+var name = '황성일';
+
+var letter = 'Dear '+name+' Culpa dolor esse irure minim id. Occaecat amet sit nisi quis qui minim nulla officia occaecat irure ut nulla eu. Nisi ex voluptate commodo non deserunt reprehenderit in laborum non dolore exercitation ullamco consectetur proident. Tempor elit minim ad pariatur '+name+' ullamco magna ad Lorem. Nisi labore tempor aute laborum dolore nostrud consequat voluptate aute Lorem Lorem est reprehenderit Esse ad eiusmod consectetur consectetur nulla ex occaecat excepteur culpa quis laborum ullamco. Sint enim dolor labore aliquip adipisicing '+name+' Exercitation in dolor amet ullamco labore pariatur nisi eu Magna proident magna sint minim Lorem velit. Labore minim ad pariatur fugiat ipsum duis. Magna deserunt sunt occaecat excepteur Lorem sunt culpa ullamco pariatur qui seungil esse veniam magna';
+
+console.log(letter);
